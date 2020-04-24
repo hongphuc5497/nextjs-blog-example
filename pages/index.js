8199c6e12eb725073361e58c2d9cf9c9a746aacc
Hong Phuc <hongphuc.dthp@gmail.com>
2020-04-25 00:09:58 +0700

    Dynamic Routes

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,14 @@
-import Head from "next/head";
-import Layout, { siteTitle } from "../components/layout";
+import Layout from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
 
 export default function Home({ allPostsData }) {
 	console.log(allPostsData);
-	
+
 	return (
 		<Layout home>
-			<Head>
-				<title>{siteTitle}</title>
-			</Head>
-
 			<section className={utilStyles.headingMd}>
 				<p>
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -33,11 +30,13 @@ export default function Home({ allPostsData }) {
 				<ul className={utilStyles.list}>
 					{allPostsData.map(({ id, date, title }) => (
 						<li className={utilStyles.listItem} key={id}>
-							{title}
+							<Link href="/posts/[id]" as={`/posts/${id}`}>
+								<a>{title}</a>
+							</Link>
 							<br />
-							{id}
-							<br />
-							{date}
+							<small className={utilStyles.lightText}>
+								<Date dateString={date}/>
+							</small>
 						</li>
 					))}
 				</ul>
@@ -48,6 +47,7 @@ export default function Home({ allPostsData }) {
 
 export async function getStaticProps() {
 	const allPostsData = getSortedPostsData();
+
 	return {
 		props: {
 			allPostsData
